d5ff9dc93d003c7283859ed87584967e742570d1
gabriela <gabdelc@gmail.com>
2016-12-22 12:34:44 -0500

    onchange

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-function calcular() {
-   
-    var mono1= document.getElementById("aSmile").value;
-    var mono2 = parseInt(document.getElementById("bSmile").value);
-    var aSmile = false;
-    var aSmile = false;
-    var salida = document.getElementById("salida");
-    
-   if(aSmile){
-       
-   }
+function mono1()
+{
+	var img = document.getElementById("img_mono_1");
+	var checkA = document.getElementById("mono_1");
+
+	if(checkA.checked)
+	{
+		img.innerHTML = '<img class="imagen" src="img/happy.jpg">';
+	}else{
+		img.innerHTML = '<img src="img/sad.jpg">';
+	}
+}
+
+
+
+function mono2()
+{
+	var img = document.getElementById("img_mono_2");
+	var checkB = document.getElementById("mono_2");
+
+	if(checkB.checked)
+		img.innerHTML = '<img src="img/happy.jpg">';
+	else
+		img.innerHTML = '<img src="img/sad.jpg">';
+}
+
+function evaluar()
+{
+	var aSmile = document.getElementById("mono_1").checked;
+	var bSmile = document.getElementById("mono_2").checked;
+	var salida = document.getElementById("salida");
+
+	if(aSmile == bSmile)
+	{
+		salida.innerHTML = "1";
+	}else{
+		
+		salida.innerHTML = "0";
+	}
+}
